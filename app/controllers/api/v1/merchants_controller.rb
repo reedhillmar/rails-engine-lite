5c6f5e47712e2ab325d82ff1171d64dceeec36c5
Reed Hillmar <reedhillmar@gmail.com>
2023-10-16 14:19:29 -0600

    test: adds test code for requesting all merchants

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    render json: Merchant.all
+  end
+
+  def show
+    render json: Merchant.find(params[:id])
+  end
+end
