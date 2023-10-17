4345d804323a7e007dc3924b98de9f90fffd1d2d
Reed Hillmar <reedhillmar@gmail.com>
2023-10-16 19:07:59 -0600

    test: adds test code for returning all of a merchant's items

--- spec/requests/merchants_request_spec.rb
+++ spec/requests/merchants_request_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require 'rails_helper'
 
 describe 'Merchants API' do
@@ -38,4 +40,26 @@ describe 'Merchants API' do
     expect(merchant).to have_key(:name)
     expect(merchant[:name]).to be_a(String)
   end
+
+  it "can return all of a merchant's items" do
+    merchant = create(:merchant)
+
+    create_list(:item, 4, merchant_id: merchant.id)
+
+    first = Item.first
+    second = Item.second
+    third = Item.third
+    fourth = Item.last
+
+    get "/api/v1/merchants/#{id}/items"
+
+    expect(response).to be_successful
+    expect(merchant.items.count).to eq(4)
+    expect(merchant.items.first).to eq(first)
+    expect(merchant.items.second).to eq(second)
+    expect(merchant.items.third).to eq(third)
+    expect(merchant.items.last).to eq(last)
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
